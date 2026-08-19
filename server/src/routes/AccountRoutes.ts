260dbd06fb1441e8fd086f1cd06097ca7ff4be34
adcs <168171581+BDThais@users.noreply.github.com>
2026-08-19 19:22:38 +0700

    implement rate limiter for /login

--- server/src/routes/AccountRoutes.ts
+++ server/src/routes/AccountRoutes.ts
@@ -1,10 +1,11 @@
 import Router from 'express';
 import {createAccount, login, logout, me} from '../controllers/AccountControllers';
+import { loginLimiter } from '../middlewares/RateLimiter';
 
 const accountRouter = Router();
 
 accountRouter.post('/signup', createAccount);
-accountRouter.post('/login', login);
+accountRouter.post('/login', loginLimiter, login);
 accountRouter.post('/logout', logout);
 accountRouter.get('/me', me);
 
